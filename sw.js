f93e9241aec0734b0ba7f333d3b6415aa1f6df22
HeltonTec06 <heltonjairo192@gmail.com>
2026-09-16 09:11:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gta-gps-v13-shell';
+const CACHE='gta-gps-v14-shell';
 const APP=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
